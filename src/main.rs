742d7e23359e8816d21db168915e0e62e1ea25da
shashank <shashank944533@gmail.com>
2022-12-09 10:09:06 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use reqwest::blocking::Client;
+use reqwest::Error;
+
+fn main() -> Result<(), Error>{
+    let client = Client::new();
+
+    let user = "testuser" .to_string();
+    let passwd:Option<String> = None;
+
+    let response = client 
+    .get("https://httpbin.org/get")
+    .basic_auth(user, passwd)
+    .send();
+
+    println!("{:?}", response);
+
+    Ok(())
+}
